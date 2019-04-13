c415c4dc5635c4302ecfc4b5487099fc0f7eb933
Saras Arya <saras.arya@gmail.com>
2019-04-13 15:59:10 +0530

    fixed the bug thanks to  @znck

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,11 +9,10 @@
 </template>
 
 <style lang="scss">
-
-@import url(./assets/main.scss);
+@import "./assets/main.scss";
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
